1cb472c8f85345d3df5c07466d42293630ec5b21
KishVS <kishorevsrini@gmail.com>
2022-10-02 17:00:56 +0530

    adding nse code

--- stock compare.py
+++ stock compare.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 #Author Kishore Kumaar
 #Date 13-Sep-2022
+# from bsedata.bse import BSE
+# b = BSE()
+# print(b)
+# q = b.getQuote('534976')
+# print(q)
+from email.quoprimime import quote
+from nsetools import Nse
+import pandas as pd, numpy as np
+nse = Nse()
+#quote = nse.get_quote('sbin')
+code = nse.get_stock_codes()
+print(code)
+#print(quote['companyName'])
+#print("Current Price:" + str(quote['buyPrice1']))
+q = input("Enter Stock Code:")
+quote = nse.get_quote(q)
+print(quote)
